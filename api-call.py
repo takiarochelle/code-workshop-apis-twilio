35c258f2234defb34feea3b24699fa96df0b81ac
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-17 21:11:02 -0700

    last commit

--- api-call.py
+++ api-call.py
@@ -5,15 +5,14 @@ from config import ACCOUNT_SID, AUTH_TOKEN, TWILIO_NUMBER
 
 # add one of you phone numbers here with no spaces
 # exampe: "+15553334444"
-to_number = "+1##########"
+to_number = "+17074792457"
 
 # set a variable call client to an instance of Client
-client = #your code
+client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
 # lets make and send the message, add in the missing data
-message = #your code on this line(
-                to=to_number,
-                from_=TWILIO_NUMBER,
-                body="You did it!")
+message = client.api.account.messages.create(to=to_number,
+                                             from_=TWILIO_NUMBER,
+                                             body="You did it!")
 
 print "Message set. Message Id: {}".format(message.sid)
--- send_sms.py
+++ send_sms.py
@@ -4,20 +4,28 @@ from config import ACCOUNT_SID, AUTH_TOKEN, TWILIO_NUMBER
 
 def validates_choice(user_choice):
     """Returns bool of user's choice"""
-
-    pass
-
+    if user_choice == "y" or user_choice == "yes":
+        return True
+    else:
+        return False
 
 def gets_to_number():
     """Ask user for phone number, returns it with +1 added"""
-
-    pass
-
+    user_num = raw_input("What your is your phone number?")
+    if len(user_num) != 10:
+        print "This is either has too many or too few integers."
+        main(ACCOUNT_SID, AUTH_TOKEN, TWILIO_NUMBER)
+    elif user_num.isdigit() == False:
+        print "This is not a number. Try again with using integers."
+        main(ACCOUNT_SID, AUTH_TOKEN, TWILIO_NUMBER)
+    else:
+        new_num = "+1" + str(user_num)
+        return new_num
 
 def gets_message_to_send():
     """Ask user for message to send"""
-
-    pass
+    user_message = raw_input("What would you like to send? ")
+    return user_message
 
 
 def send_message(to_number, to_message, ACCOUNT_SID,
@@ -38,7 +46,7 @@ def main(ACCOUNT_SID, AUTH_TOKEN, TWILIO_NUMBER):
 
     print "Welcome to this Twilio API Demo"
 
-    user_choice = raw_input("""Would you like to send a next message? """)
+    user_choice = raw_input("""Would you like to send a text message? """)
 
     validated_user_choice = validates_choice(user_choice)
 
